855159843c39f0cf270e44ac3b3ecdd6b47eb1f8
JulienDagnelie <julien.dagnelie@student.uclouvain.be>
2021-10-24 18:10:26 +0200

    led1

--- led.py
+++ led.py
@@ -1,5 +1,15 @@
 from machine import Pin
+import time
 
 led = Pin(0, Pin.OUT)
-
 led.value(1)
+x = input("Entre 0/1 pour allumer ou eteindre la lampe:")
+if x ==0:
+    print("J'éteint la led")
+    time.sleep(1)
+    led.value(0)
+if x == 1:
+    print("J'allume la led")
+    time.sleep(1)
+    led.value(1)
+    
