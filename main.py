e41bec652756f3a242d7c52af281e4ad89ee1659
Loïc <loic.tumelaire@gmail.com>
2022-01-05 11:30:44 +0100

    start

--- main.py
+++ main.py
@@ -3,9 +3,25 @@ import time
 
 pin = Pin(25, Pin.OUT)
 
-while True:
-    pin.toggle()
-    time.sleep_ms(1000)
+# while True:
+#     pin.toggle()
+#     time.sleep_ms(1000)
+
+run  = True 
+while run:
+    x = input("Entre 0/1/2pour allumer, eteindre la lampe ou stopper le système:")
+    if x == 0:
+        print("J'allume la led")
+        pin.toggle()
+    elif x == 1:
+        print("J'éteint la led")
+        time.sleep(1)
+        led.value(x)
+    elif x == 2:
+        print("Eteignons la led de l'arduino")
+        time.sleep(5)
+        led.value(0)
+        run = False 
 
 '''rom machine import Pin
 import sys, time
