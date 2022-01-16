c7ffa9cf7bb5e1f10b6adb47dcebd719ee32259f
Loïc <loic.tumelaire@gmail.com>
2022-01-16 15:43:47 +0100

    start

--- Code python/LCD.py
+++ Code python/LCD.py
@@ -2,15 +2,15 @@
 File: LCD.py
 Project: Code python
 Created Date: Su Jan 2022
-Author: Julien Dagnelie
+Author: Julien Dagnelie & Loïc Tumelaire
 -----
 Last Modified: Sun Jan 16 2022
-Modified By: Julien Dagnelie
+Modified By: Julien Dagnelie & Loïc Tumelaire
 -----
 Copyright (c) 2022 Universite catholique de Louvain
 -----
 HISTORY:
-Date   Sun Jan 16 2022   	By Julien Dagnelie	Comments
+Date   Sun Jan 16 2022   	By Julien Dagnelie & Loïc Tumelaire	Comments
 ----------	---	---------------------------------------------------------
 '''
 
