bce985281e57d44c973092faacc724616af56977
Julien <julien.dagnelie@student.uclouvain.be>
2022-01-16 14:38:17 +0100

    Fonction distance

--- Code python/GPS.py
+++ Code python/GPS.py
@@ -13,3 +13,45 @@ HISTORY:
 Date   Sun Jan 16 2022   	By Julien Dagnelie	Comments
 ----------	---	---------------------------------------------------------
 '''
+import machine
+import time 
+from micropyGPS import MicropyGPS
+from math import *
+
+uart = machine.UART(1, baudrate=9600, tx = machine.Pin(1))
+gps = MicropyGPS()
+
+def start():
+    pass
+
+def distance(lat1,lat2, long1, long2):
+    """Calcul la distance entre deux points grace a la formule haversine
+
+    Args:
+        lat1 [int]: Latitude des points du point de depart
+        lat2 [int]: Latitude des points du point d'arrivee
+        long1 [int]: Longitude des points du point de depart
+        long2 [int]: Longitude des points du point d'arrivee
+
+    Returns:
+        distkm [int]: distance entre les deux point (lat1, long1) et (lat2, long2)
+    """
+    R = 6371000 # Rayon de la terre [m]
+
+    phi1, phi2 = radians(lat1), radians(lat2)
+
+    delta_phi = radians(phi2 - phi1)
+    delta_lambda = radians(long2-long1)
+
+    # Calcul grace a la formule haversine c.f Wikipedia
+
+    a = sin(delta_phi / 2.0) ** 2 + cos(phi1) * cos(phi2) * sin(delta_lambda / 2.0) ** 2
+    
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distm = R * c # distance [m]
+    distkm = round(distm / 1000, 3) # distance [km]
+
+    return distkm
+    
+
