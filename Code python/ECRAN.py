fa7f6b2a42f32770b0c7d18f2db9f8985b35c8ea
Loïc <loic.tumelaire@gmail.com>
2022-01-16 18:32:14 +0100

    ecran

---
+++ Code python/ECRAN.py
@@ -0,0 +1,18 @@
+'''
+File: ECRAN.py
+Project: Code python
+Created Date: Su Jan 2022
+Author: Julien Dagnelie & Loïc Tumelaire
+-----
+Last Modified: Sun Jan 16 2022
+Modified By: Julien Dagnelie & Loïc Tumelaire
+-----
+Copyright (c) 2022 Universite catholique de Louvain
+-----
+HISTORY:
+Date   Sun Jan 16 2022   	By Julien Dagnelie & Loïc Tumelaire	Comments
+----------	---	---------------------------------------------------------
+'''
+
+from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
