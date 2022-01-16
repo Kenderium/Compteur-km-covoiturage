48d4cc8ee0e2abfba2c8bddf2b630739877e311c
Julien <julien.dagnelie@student.uclouvain.be>
2022-01-16 17:00:30 +0100

    save

---
+++ Code python/ECRAN.py
@@ -0,0 +1,16 @@
+'''
+File: ECRAN.py
+Project: Code python
+Created Date: Su Jan 2022
+Author: Julien Dagnelie & Loïc Tumelaire
+-----
+Last Modified: Sun Jan 16 2022
+Modified By: Julien Dagnelie & Loïc Tumelaire
+-----
+Copyright (c) 2022 Universite catholique de Louvain
+-----
+HISTORY:
+Date   Sun Jan 16 2022   	By Julien Dagnelie & Loïc Tumelaire	Comments
+----------	---	---------------------------------------------------------
+'''
+
