1014d670973fd5691207f40f22eeebafa0bfc9aa
Loïc <loic.tumelaire@gmail.com>
2022-01-17 21:48:12 +0100

    RFID

--- Code python/RFID.py
+++ Code python/RFID.py
@@ -4,8 +4,8 @@ Project: Code python
 Created Date: Su Jan 2022
 Author: Julien Dagnelie
 -----
-Last Modified: Sun Jan 16 2022
-Modified By: Julien Dagnelie
+Last Modified: Mon Jan 17 2022
+Modified By: Julien Dagnelie & Loïc Tumelaire
 -----
 Copyright (c) 2022 Universite catholique de Louvain
 -----
@@ -13,4 +13,6 @@ HISTORY:
 Date   Sun Jan 16 2022   	By Julien Dagnelie	Comments
 ----------	---	---------------------------------------------------------
 '''
+from mfrc522 import MFRC522
 
+reader = MFRC522(spi_id=0,sck=2,miso=4,mosi=3,cs=6,rst=5)  #spi_id =?
