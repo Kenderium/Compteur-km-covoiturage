40172e5d4a8613caa251f5900d92e30c03447333
JulienDagnelie <julien.dagnelie@student.uclouvain.be>
2022-02-01 16:28:12 +0100

    Main.py

--- Code python/Main.py
+++ Code python/Main.py
@@ -16,12 +16,13 @@ Date   Sun Jan 16 2022   	By Julien Dagnelie	Comments
 
 import machine
 import time
-
 import GPS1
 import ECRAN
 import RFID
 import covoit
 
+
+#  Creation des boutons, led indicatrices
 bouton1 = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN) # bouton suivant
 bouton2 = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_DOWN) # bouton valider
 led_verte = machine.Pin(17, machine.Pin.OUT)
@@ -30,6 +31,10 @@ led_rouge = machine.Pin(16, machine.Pin.OUT)
 Conducteur = ""
 Passagers = []
 trajet_numero = 0
+
+# ------------------------------
+# Fonctions menus, et run ------
+# ------------------------------
 
 def bonjour():
     """ Message de bienvenue
@@ -42,6 +47,7 @@ def bonjour():
     time.sleep(1)
     led_verte.value(0)
     led_rouge.value(0)
+
 def aurevoir():
     """ Message d'aurevoir
     """
@@ -59,26 +65,31 @@ def menu1():
     """
     ECRAN.txt("Encoder chauffeur", 1, 1)
     ECRAN.afficher()
+
 def menu2():
     """ Menu 2: encodage passager
     """
     ECRAN.txt("Encoder passagers", 1, 1)
     ECRAN.afficher()
+
 def menu3():
     """ Menu 3: démarrage du voyage
     """
     ECRAN.txt("Démarrer le voyage", 1, 1)
     ECRAN.afficher()
+
 def menu4():
     """ Menu 4: historique
     """
     ECRAN.txt("Historique", 1, 1)
     ECRAN.afficher()
+
 def menu5():
     """ Menu 5: Bluethoot
     """
     ECRAN.txt("Bluethoot", 1, 1)
     ECRAN.afficher()
+
 def menu_exit():
     """ Menu_exit: exit
     """
@@ -96,17 +107,21 @@ def run():
 
 
 if __name__ == "main":
+    
     if bouton1.value():                             # Allumage
         bonjour()                                   # Message de bienvenue
+
         while True:
             i = 1
             if i == 1:                              # Encoder conducteur
+
                 menu1()
                 if bouton2.value():                 # Confirmation
                     led_verte.value(1)
                     time.sleep(0.5)
                     led_verte.value(0)
                     Conducteur = RFID.lecture()     # Encodage conducteur
+
             if i == 2:                              # Encoder passagers
                 menu2()
                 if bouton2.value():                 # Confirmation
@@ -118,6 +133,7 @@ if __name__ == "main":
                         led_rouge.value(1)
                         time.sleep(0.5)
                         led_rouge.value(0)
+
             if i == 3:                              # Démarer voyage
                 menu3()
                 if bouton2.value():                 # Confirmation
@@ -125,6 +141,7 @@ if __name__ == "main":
                     time.sleep(0.5)
                     led_rouge.value(0)
                     run()                           # Démarage voyage
+
             if i == 4:
                 menu4()                             # Historique
                 if bouton2.value():                 # Confirmation
@@ -141,6 +158,7 @@ if __name__ == "main":
                                 ECRAN.afficher()
                             except IndexError:
                                 break
+
             if i == 5:
                 menu5()                             # Bluethoot
                 if bouton2.value():                 # Confirmation
@@ -148,6 +166,7 @@ if __name__ == "main":
                     time.sleep(0.5)
                     led_verte.value(0)
                     pass
+
             if i == 6:                              # Menu exit
                 menu_exit()
                 if bouton2.value():                 # Confirmation
@@ -155,6 +174,7 @@ if __name__ == "main":
                     time.sleep(0.5)
                     led_rouge.value(0)
                     aurevoir()
+
             if bouton1.value():                     # Changer de menu
                 if i == 6:
                     i = 1
