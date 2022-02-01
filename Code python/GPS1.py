2f5a8f2278b70df649e662cf36e53e4bf7169882
Loïc <loic.tumelaire@gmail.com>
2022-02-01 15:48:15 +0100

    v1

--- Code python/GPS1.py
+++ Code python/GPS1.py
@@ -4,7 +4,7 @@ Project: Code python
 Created Date: Su Jan 2022
 Author: Julien Dagnelie
 -----
-Last Modified: Mon Jan 31 2022
+Last Modified: Tue Feb 01 2022
 Modified By: Julien Dagnelie & Loïc Tumelaire
 -----
 Copyright (c) 2022 Universite catholique de Louvain
@@ -18,33 +18,43 @@ from micropyGPS import MicropyGPS
 gps = MicropyGPS()
 from math import radians, acos, cos, sin
 import time
-#import os
-import _thread                                              #Pour exécuter plusieures taches en simultané
+import _thread                                              # Pour exécuter plusieures taches en simultané
 
-uart = machine.UART(0, baudrate=9600, tx = machine.Pin(0))  #Pin du GPS
+baton = _thread.allocate_lock()                             # Bloqueur de thread pour éviter crash,...
+uart = machine.UART(0, baudrate=9600, tx = machine.Pin(0))  # Pin du GPS
+on = True
 
-def startgps(running=False):
+def startgps(running):
     """Demarre le tracking gps et logs les latitudes et longitudes dans le fichier de logs
 
     Args:
         running (bool, optional): . Defaults to False.
     """
 
-    gps.start_logging("logs.txt")    # Base de données meilleure ?
+    gps.start_logging("logs.txt")                       # Base de données meilleure ?
     while running:
         if uart.any():
-            print("nani")                               # La boucle n'est réalisée que 2 fois ? --> bug
             coordonees = gps.latitude, gps.longitude
             gps.write_log(str(coordonees) + "\n")  
         time.sleep(1.5)
-        running = False 
     gps.stop_logging()  
 
+def second_thread():        # Seconde tâche: Si on appuie sur le bouton
+        baton.acquire()     # Regarde si le thread est libre et se l'acquière
+        global on
+        time.sleep(5)
+        bouton = True       # On appuie sur le bouton "A modifier"
+        if bouton:
+            on = False      # On arrete le Gps
+        baton.release()     # Libère le thread
+
+"""
+INUTILE ?
 
 def transformation_coord(coord): 
     new_coord = coord[0] + coord[1] / 60
     return new_coord
-    
+"""
 
 def distance(coord1, coord2):
     """Calcul la distance entre 2 point avec les latitudes et longitudes
@@ -74,49 +84,32 @@ def distance(coord1, coord2):
 
     return distkm
 
-compteur_km = True           # Démarre le calcul des km
-
-
 def run():
-    baton = _thread.allocate_lock()               # Bloqueur de thread pour éviter crash,...
-
-    def second_thread():         # Seconde tâche
-        global compteur_km
-        while compteur_km:
-            baton.acquire()      # Regarde si le thread est libre et se l'acquière
-            startgps(True)       # Démarre le gps   --> #Bug
-            startgps(False)      # Arrete le gps
-            print(km)            # Affiche les km parcourus
-            compteur_km = False  # Arrete le calcul des km
-            
-            baton.release()      # Libère le thread
-    
+    global on
     _thread.start_new_thread(second_thread, ())   # Création du thread et démarrage
+    
+    while on:  
+        startgps(True)              # Démarre le gps
+    startgps(False)                 # Arrete le gps
 
-    time.sleep(1.5)
-
-    while compteur_km:
-        with open("logs.txt") as file:  # Ouvre le fichier des trajets
-            l = []                      # Liste contenant comme chaque élément une coordonnée: ([0, 0.0, 'N'], [0, 0.0, 'W'])
-            d=[]                        # Liste finale, avec juste longitude, lat (sans "(" ")"
-            km = 0                      # Compteur de km
+    with open("logs.txt") as file:  # Ouvre le fichier des trajets
+        l = []                      # Liste contenant comme chaque élément une coordonnée: ([0, 0.0, 'N'], [0, 0.0, 'W'])
+        d = []                      # Liste finale, avec juste longitude, lat (sans "(" ")"
+        km = 0                      # Compteur de km
+        
+        for line in file:
+            l.append(line.strip())  # Ajoute les lignes du fichier dans une liste
             
-            for line in file:
-                l.append(line.strip())  # Ajoute les lignes du fichier dans une liste
-                
-            for c in range(len(l)):
-                element = ""
-                for char in l[c]:
-                    if char != "(" and char != ")":
-                        element += char
-                d.append(element)                   # Création de la liste d
-                
-                time.sleep(1.5)                     # Attendre que la voiture change de coordonnées
-                
-                try:
-                    km += distance(d[c],d[c+1])     # Somme distance entre chaque point
-                except IndexError:
-                    pass
-                
-                print("test_th1")
-run()
+        for c in range(len(l)):
+            element = ""
+            for char in l[c]:
+                if char != "(" and char != ")":
+                    element += char
+            d.append(element)                   # Création de la liste d
+            try:
+                km += distance(d[c],d[c+1])     # Somme distance entre chaque point
+            except IndexError:
+                pass
+    return km
+
+print(run())
