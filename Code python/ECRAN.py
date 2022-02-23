16a23d901e7b8547dbcbae2e5a31444a11dc668f
Loïc <loic.tumelaire@gmail.com>
2022-02-23 15:48:22 +0100

    ecran

--- Code python/ECRAN.py
+++ Code python/ECRAN.py
@@ -16,7 +16,6 @@ Date   Sun Jan 16 2022   	By Julien Dagnelie & Loïc Tumelaire	Comments
 
 from machine import Pin, I2C
 import ssd1306py
-#ssd1306py.ssd1306.SSD1306_I2C
 
 i2c=I2C(1,sda=Pin(26), scl=Pin(27), freq=400000)
 oled = ssd1306py.ssd1306.SSD1306_I2C(128, 64, i2c)
@@ -31,11 +30,6 @@ def txt(texte, x, y):
     """
     oled.text(texte, x, y)
 
-def afficher():
-    """Pour afficher les textes réalisés à l'écran.
-    """
-    oled.show()
-
 def pixel(x, y):
     """Créée un pixel.
 
@@ -45,32 +39,9 @@ def pixel(x, y):
     """
     oled.pixel(x,y,1)
 
-def line(x1, y1, x2, y2):
-    """Créee une ligne (diagonale, verticale, horizontale)
-
-    Args:
-        x1 (int): Coordonnée x début
-        y1 (int): Coordonnée y début
-        x2 (int): Coordonnée x fin
-        y2 (int): Coordonnée y fin
+def afficher():
+    """Pour afficher les textes réalisés à l'écran.
     """
-    oled.line(x1,y1,x2,y2,1)
-    pass
-
-def rectangle(x1, y1, x2, y2, fill=False):
-    """Créée un rectangle, remplis ou pas
-
-    Args:
-        x1 (int): Coordonnée x début
-        y1 (int): Coordonnée y début
-        x2 (int): Coordonnée x fin
-        y2 (int): Coordonnée y fin
-        fill (bool, optional): Remplir le rectangle. Defaults to False.
-    """
-    oled.rect(x1, y1, x2, y2, 1)
-    pass
-    if fill == True:
-        oled.fill_rect(x1, y1, x2, y2, 1)
-        pass
+    oled.show()
 
 #Images et défilements inutiles?
