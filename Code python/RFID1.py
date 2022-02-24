edc099df47bf0c16c43f0fb2de333aa9ff531e4e
Kenderium <loic.tumelaire@gmail.com>
2022-02-24 23:34:36 +0100

    RFID.v2

--- Code python/RFID1.py
+++ Code python/RFID1.py
@@ -18,15 +18,17 @@ from mfrc522 import MFRC522
 reader = MFRC522(spi_id=0,sck=2,miso=4,mosi=3,cs=6,rst=5)       # spi_id =?
 
 def lecture():
-    while True:
-        reader.init()                                           # Start the connection to the RFID reader
-        (stat, tag_type) = reader.request(reader.REQIDL)        # Request the current status of the reader. Nani ?
-        if stat == reader.OK:                                   # Checks the value stored in stat, if the reader is ok, the code moves forward.
-            (stat, uid) = reader.SelectTagSN()
-            card = int.from_bytes(bytes(uid), "little", False)  # Card store the data from an RFID card / tag
-            return(card)                                        # Print the card details to the Python shell
-            #if card == 611994825:                              # Si la carte est le numéro ...
-                #print("Hello user1")
+    """ Lire le numéro de la carte
+    """
+    #while True:
+    reader.init()                                           # Start the connection to the RFID reader
+    (stat, tag_type) = reader.request(reader.REQIDL)        # Request the current status of the reader. Nani ?
+    if stat == reader.OK:                                   # Checks the value stored in stat, if the reader is ok, the code moves forward.
+        (stat, uid) = reader.SelectTagSN()
+        card = int.from_bytes(bytes(uid), "little", False)  # Card store the data from an RFID card / tag
+        return(card)                                        # Print the card details to the Python shell
+        #if card == 611994825:                              # Si la carte est le numéro ...
+            #print("Hello user1")
 
 def name(card):
     """Retourne ne nom du propriétaire de la carte
