bdd7ebf7e9c68cead0c624b5fc20a1c0f5fff584
Loïc <loic.tumelaire@gmail.com>
2022-03-23 17:35:41 +0100

    covoit start

--- Code python/covoit.py
+++ Code python/covoit.py
@@ -14,26 +14,24 @@ Date   Sun Jan 16 2022   	By Julien Dagnelie	Comments
 ----------	---	---------------------------------------------------------
 '''
 def prix(historique, Conducteur, Passagers, prix_plein= 16 ):
-    
-    try:
-        list_passager = passagers
-        km_perso = float(input("Quel est ton kilometrage personel ? : "))
-        km_tot = km_perso
+    with open("historique_trajets.txt") as historique:             #Enregistrer le trajet (km + conducteur + passagers)
 
-        for i in range(len(passagers)):
-            x = i+1
-            passager = float(input("Entrez le nombre de kilomètre du passager n°{} : ".format(x)))
-            list_passager.append(passager)
-            km_tot += passager
-            
-        for k in range(4):
-            z = k + 1
-            prix  = (list_passager[k]/km_tot)*prix_plein
-            print("Le passager {} doit payer ".format(z), round(prix, 2), "€", sep="")
-    except ValueError:
-        print("Un plein ou des kilomètres ne se fait pas avec des lettres mais bien des chiffres")
-    finally:
-        print("C'est ici que se termine mon beau calcul")
-input()
+        try:
+            list_passager = passagers
+            km_perso = float(input("Quel est ton kilometrage personel ? : "))
+            km_tot = km_perso
 
-print(prix(10,30,10,15))
+            for i in range(len(passagers)):
+                x = i+1
+                passager = float(input("Entrez le nombre de kilomètre du passager n°{} : ".format(x)))
+                list_passager.append(passager)
+                km_tot += passager
+                
+            for k in range(4):
+                z = k + 1
+                prix  = (list_passager[k]/km_tot)*prix_plein
+                print("Le passager {} doit payer ".format(z), round(prix, 2), "€", sep="")
+        except ValueError:
+            print("Un plein ou des kilomètres ne se fait pas avec des lettres mais bien des chiffres")
+        finally:
+            print("C'est ici que se termine mon beau calcul")
