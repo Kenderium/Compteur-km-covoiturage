f73cb47df2bcb9270431def2302e4b2d2e5cbd8f
Julien Dagnelie <julien.dagnelie@student.uclouvain.be>
2023-11-15 12:33:46 +0100

    GPS

--- Code python/GPS1.py
+++ Code python/GPS1.py
@@ -17,23 +17,24 @@ import machine
 from micropyGPS import MicropyGPS
 from math import radians, acos, cos, sin
 import time
-import _thread                                              # Pour exécuter plusieures taches en simultané
+import _thread  # Pour exécuter plusieures taches en simultané
 
-uart = machine.UART(1, baudrate=9600, tx = machine.Pin(8), rx = machine.Pin(9))
+uart = machine.UART(1, baudrate=9600, tx=machine.Pin(8), rx=machine.Pin(9))
 gps = MicropyGPS()
+
 
 def startgps(start):
     """Demarre le tracking gps et logs les latitudes et longitudes dans le fichier de logs
 
     Args:
-        running (bool, optional): . Defaults to False.
+        start (bool, optional): . Defaults to False.
     """
 
-    gps.start_logging("logs.txt")                       # Base de données meilleure ?
+    gps.start_logging("CoordinateLogs.txt")  # Base de données meilleure ?
     if uart.any():
         coordonees = gps.latitude, gps.longitude
-        gps.write_log(str(coordonees) + "\n")  
-    time.sleep(1.5)
+        gps.write_log(str(coordonees) + "\n")
+    time.sleep(1.5)  # Toute les 1.5 secondes suffisant ?
     gps.stop_logging()
 
 
@@ -45,6 +46,7 @@ def transformation_coord(coord):
     return new_coord
 """
 
+
 def distance(coord1, coord2):
     """Calcul la distance entre 2 point avec les latitudes et longitudes
 
@@ -55,63 +57,65 @@ def distance(coord1, coord2):
     Returns:
         distkm: [description]
     """
-  
-    R = 6378137 # Rayon de la terre [m]
-    
+
+    R = 6378137  # Rayon de la terre [m]
+
     phi1, phi2 = radians(float(coord1[0])), radians(float(coord2[0]))
     lambda1, lambda2 = radians(float(coord1[0])), radians(float(coord2[1]))
 
     delta_phi = radians(phi2 - phi1)
-    delta_lambda = radians(lambda2-lambda1)
+    delta_lambda = radians(lambda2 - lambda1)
 
     # Calcul grace a la formule haversine c.f Wikipedia
 
-    S = acos(sin(phi1)*sin(phi2) + cos(lambda1)*cos(lambda2)*cos(delta_lambda))
+    S = acos(sin(phi1) * sin(phi2) + cos(lambda1) * cos(lambda2) * cos(delta_lambda))
 
-    distm = R * S                   # distance [m]
-    distkm = round(distm / 1000, 3) # distance [km]
+    distm = R * S  # distance [m]
+    distkm = round(distm / 1000, 3)  # distance [km]
 
     return distkm
 
+
 def run():
-    _thread.start_new_thread(second_thread, ())   # Création du thread et démarrage
+    _thread.start_new_thread(second_thread, ())  # Création du thread et démarrage
     global go
     while go:
-        startgps(True)              # Démarre le gps
-    startgps(False)                 # Arrete le gps
+        startgps(True)  # Démarre le gps
+    startgps(False)  # Arrete le gps
 
-    with open("logs.txt") as file:  # Ouvre le fichier des trajets
-        l = []                      # Liste contenant comme chaque élément une coordonnée: ([0, 0.0, 'N'], [0, 0.0, 'W'])
-        d = []                      # Liste finale, avec juste longitude, lat (sans "(" ")"
-        km = 0                      # Compteur de km
-        
+    with open("CoordinateLogs.txt") as file:  # Ouvre le fichier des trajets
+        l = []  # Liste contenant comme chaque élément une coordonnée: ([0, 0.0, 'N'], [0, 0.0, 'W'])
+        d = []  # Liste finale, avec juste longitude, lat (sans "(" ")"
+        km = 0  # Compteur de km
+
         for line in file:
             l.append(line.strip())  # Ajoute les lignes du fichier dans une liste
-            
+
         for c in range(len(l)):
             element = ""
             for char in l[c]:
                 if char != "(" and char != ")":
                     element += char
-            d.append(element)                   # Création de la liste d
+            d.append(element)  # Création de la liste d
             try:
-                km += distance(d[c],d[c+1])     # Somme distance entre chaque point
+                km += distance(d[c], d[c + 1])  # Somme distance entre chaque point
             except IndexError:
                 pass
             print(km)
     return km
 
-def second_thread():        # Seconde tâche: Si on appuie sur le bouton
+
+def second_thread():  # Seconde tâche: Si on appuie sur le bouton
     global go
     bouton = True
     baton = _thread.allocate_lock()
     while bouton:
-        baton.acquire()     # Regarde si le thread est libre et se l'acquière
+        baton.acquire()  # Regarde si le thread est libre et se l'acquière
         time.sleep(50)
-        bouton = False       # On appuie sur le bouton "A modifier"
+        bouton = False  # On appuie sur le bouton "A modifier"
         if bouton == False:
             go = False
-        baton.release()     # Libère le thread
+        baton.release()  # Libère le thread
 
 
 go = True
